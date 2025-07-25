7cc8e10a642d9c51d21920732ffc2a8e3f0fb4d8
Kevin Nennig <knennig213@gmail.com>
2025-07-25 00:11:26 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -70,11 +70,10 @@ function createContentFragmentDisplay(contentFragment) {
   const container = document.createElement('div');
   container.className = 'content-fragment-detail';
   // eslint-disable-next-line no-underscore-dangle
-  // container.setAttribute('data-aue-resource',
-  // `urn:aemconnection:${contentFragment._path}/jcr:content/data/master`);
-  // container.setAttribute('data-aue-type', 'reference');
-  // container.setAttribute('data-aue-label', `${contentFragment.title}`);
-  // container.setAttribute('data-aue-filter', 'cf');
+  container.setAttribute('data-aue-resource', `urn:aemconnection:${contentFragment._path}/jcr:content/data/master`);
+  container.setAttribute('data-aue-type', 'reference');
+  container.setAttribute('data-aue-label', `${contentFragment.title}`);
+  container.setAttribute('data-aue-filter', 'cf');
 
   // Hero section with image and title
   const heroSection = document.createElement('div');
@@ -303,16 +302,6 @@ export default async function decorate(block) {
     showEmpty(block);
     return;
   }
-
-  // Add Universal Editor instrumentation to the block itself
-  // block.setAttribute('data-aue-resource', `urn:aemconnection:${cfPath}/jcr:content/data/master`);
-  // block.setAttribute('data-aue-type', 'resource');
-  // block.setAttribute('data-aue-filter', 'cf');
-  // block.setAttribute('data-aue-label', `${cfPath}`); //Sets Properties title in UE
-  block.setAttribute('data-aue-resource', `urn:aemconnection:${cfPath}/jcr:content/data/master`);
-  block.setAttribute('data-aue-type', 'reference');
-  // container.setAttribute('data-aue-label', ``);
-  block.setAttribute('data-aue-filter', 'cf');
 
   // Initial render
   await renderContentFragment(block, cfPath);
