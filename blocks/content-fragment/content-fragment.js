296542ce12ca27172dc187a40083df4fdc7f1339
Kevin Nennig <knennig213@gmail.com>
2025-07-25 00:41:32 -0400

    Update content-fragment.js

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -70,8 +70,9 @@ function createContentFragmentDisplay(contentFragment) {
   const container = document.createElement('div');
   container.className = 'content-fragment-detail';
   // eslint-disable-next-line no-underscore-dangle
+  // Required for CF Editor icon and Content Fragment fields to show up in Properties
   container.setAttribute('data-aue-resource', `urn:aemconnection:${contentFragment._path}/jcr:content/data/master`);
-  // container.setAttribute('data-aue-type', 'reference'); TODO: I dont think this is needed
+  container.setAttribute('data-aue-type', 'reference');
   container.setAttribute('data-aue-label', `${contentFragment.title}`);
   // container.setAttribute('data-aue-filter', 'cf'); TODO: I dont think this is needed
 
